1da0944c7bb06d302ded8938ff0341cbc15a1908
eduardonk9999 <thiago.novak7@gmail.com>
2023-12-06 17:24:05 -0300

    feat: entity todo

---
+++ src/test/java/br/com/todoList/TODO/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todoList.TODO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
